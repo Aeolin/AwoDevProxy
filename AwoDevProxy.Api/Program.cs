75a98983d769893e4c85998e053e9a453d1328fb
Aeolin Ferjünnoz <aeolin@aeol.in>
2024-04-16 00:57:58 +0200

    - remove https redirect for docker

--- AwoDevProxy.Api/Program.cs
+++ AwoDevProxy.Api/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseWebSockets();
 app.UseMiddleware<ProxyRootingMiddleware>();
 
