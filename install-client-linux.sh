16fcb4db9382f7f8ff6aa37a88ff3ffe42b60900
Aeolin Ferjünnoz <aeolin@aeol.in>
2024-04-21 18:45:27 +0200

    - fixed issue in installer

--- install-client-linux.sh
+++ install-client-linux.sh
@@ -1,4 +1,4 @@
 git pull
 cd AwoDevProxy.Client
 dotnet publish -c Release
-cp -f ./bin/Release/net8.0/linux-x64/publish/AwoDevProxy.Client ~/.local/bin/devprxy
+cp -f ./bin/Release/net8.0/linux-x64/publish/AwoDevProxy.Web.Client ~/.local/bin/devprxy
